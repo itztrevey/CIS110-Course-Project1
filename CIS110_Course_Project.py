c7ec9ed36d2d196f24d6d907211ec5d81dc69df4
username <useremail>
2023-06-05 15:38:23 -0700

    CIS110_Course_Project

--- CIS110_Course_Project.py
+++ CIS110_Course_Project.py
@@ -1,1 +1,43 @@
+print("Hello, I have a great story about my dog.")
+print("I have a few questions that you must answer first.")
+print("After you answer the question type any key.")
+location = input("Where should Astro be exploring: ")
+obstacle = input("What pbstacle should Astro encounter in his adventure: ")
+breed = input("\nWhat breed is Astro: ")
+reward = input("What should Astro find at the end of his adventure: ")
+logstability = input("Should the log Astro find be stable or unstable: ")
+print(f"\nOnce upon a time there was a dog {breed}. ")
+print(f"While walking in the {location} he came across a {obstacle}. ")
+print(f"While in the {location} he will get a {reward} when he leaves. ")
+print(f"The dog is trying to figure out the {location}. ")
+keepExploringDoggyLand = input(f"\nShould Astro keep exploring doggy land? Type yes or no: ")
+if keepExploringDoggyLand == "yes":
+    print(f"\nAstro keep exploring doggy land. ")
+    print(f"Astro found friendly dogs. ")
+    print(f"The dogs invited Astro to go play fetch. ")
+else:
+    print(f"\nAstro did not go down doggy land. ")
+    print(f"Astro turned around and started going back home. ")
+    print(f"On the way back they found the came across their owners. ")
+shouldAstroJumpOnTheFloatingLog = input(f"\nShould Astro jump on the floating log? Type yes or no: ")
+if shouldAstroJumpOnTheFloatingLog == "yes":
+    print(f"\nAstro jumps on the log. ")
+    print(f"The log starts to tilt and become unstable. ")
+    print(f"Astro was able to leap off the log. ")
+    print(f"He swam back to the riverbank. ")
+else:
+    print(f"\nAstro did not attempt to cross the river. ")
+    print(f"Astro waited on his owner to find a alternate route. ")
+    print(f"They came across a shallow part of the river and crossed. ")
+if keepExploringDoggyLand == "yes"
+    print(f"\nAstro keep exploring doggy land. ")
+    print(f"Astro found a hidden chamber deep onto the land. ")
+    print(f"Astro found endless supply of dog treats. ")
+    print(f"It was a dream come true for Astro. ")
+else:
+    print(f"\nAstro returned to his owner and was happy. ")
+    print(f"Astro feels grateful for the safety and love provided by his owner. ")
+    print(f"He knew a home is a place where he is always welcome. ")
+    
+
 
